b3356c2ad4f46ddee739f1b84e6bb3d5ea6c61ad
simonecantori <93288208+simonecantori@users.noreply.github.com>
2024-01-27 13:47:05 +0100

    Update loader.py

--- Zenodo/loader.py
+++ Zenodo/loader.py
@@ -3,5 +3,5 @@ import numpy as np
 N = 30 #Number of qubits
 
 # We load 1000 circuits with N qubits and P layers of gates
-thetas = np.load(f'Npy_zenodo/theta_N12N14_to_N{N}.npy') # array with shape (1000,N,1). They are the N angles of each circuit (repeated P times for each layer)
-preds = np.load(f'Npy_zenodo/pred_N12N14_to_N{N}.npy') # array with shape (1000,1). They are the predicted M_z for each circuit.
+thetas = np.load(f'Npys/theta_N18N20_to_N{N}.npy') # array with shape (1000,N,1). They are the N angles of each circuit (repeated P times for each layer)
+preds = np.load(f'Npys/pred_N18N20_to_N{N}.npy') # array with shape (1000,1). They are the predicted M_z for each circuit.
